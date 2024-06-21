a70077b1730bca3f2cf17af3d1d59b9dc99fe93e
osama2jz <mosama4u@gmail.com>
2024-06-21 15:31:10 +0500

    chore: added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import Image from "next/image";
+import logo from "../../public/logo.png";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.footer}>
+      <div className={styles.logo}>
+        <Image src={logo} alt="logo" width={200} />
+        <h2>Study Work Visa Guide</h2>
+      </div>
+      <div className={styles.visa}>
+        <p>Study Visa</p>
+        <div>
+          {Array(20)
+            .fill()
+            .map((x) => (
+              <p className={styles.country}>Pakistan</p>
+            ))}
+        </div>
+        <p>Work Visa</p>
+        <div>
+          {Array(20)
+            .fill()
+            .map((x) => (
+              <p className={styles.country}>Pakistan</p>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
